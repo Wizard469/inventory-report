ffb5637c34151dd07986a07c9bed45a1f82f6f7d
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2023-03-06 09:24:49 -0300

    Req 1 - Testing the constructor of the products' object

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,25 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+from datetime import datetime as dt
+
+start_date = dt.now().date()
+end_date = start_date.replace(start_date.year + 1)
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    new_product = Product(
+        1,
+        "Barabam",
+        "Xablau industries",
+        start_date,
+        end_date,
+        "126547853156589",
+        "The way you want to",
+    )
+
+    assert new_product.id == 1
+    assert new_product.nome_do_produto == "Barabam"
+    assert new_product.nome_da_empresa == "Xablau industries"
+    assert new_product.data_de_fabricacao == str(start_date)
+    assert new_product.data_de_validade == str(end_date)
+    assert new_product.numero_de_serie == "126547853156589"
+    assert new_product.instrucoes_de_armazenamento == "The way you want to"
