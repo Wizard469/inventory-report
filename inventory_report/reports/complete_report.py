84042aea6f3e3a2c6bf7b61965d697a9ec4268fb
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2023-03-06 14:55:16 -0300

    Req 3 - Generate a complete version of the report

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,21 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(report):
+        simple_report = SimpleReport.generate(report)
+
+        companies = Counter(product["nome_da_empresa"] for product in report)
+
+        format = ["Produtos estocados por empresa:\n"]
+
+        for company in companies:
+            format.append(f"- {company}: {companies[company]}\n")
+
+        formatted = "".join(format)
+
+        return (
+            f"{simple_report}\n"
+            f"{formatted}"
+        )
